4925009842561cf0ea4ec422803b20a09160c590
ねむけ <pleasurazy@gmail.com>
2024-10-16 19:02:41 +0800

    fix(@aqzhyi/google-analytics): simplified directory structure and optimized package export paths

--- @aqzhyi/google-analytics/astro.config.mjs
+++ @aqzhyi/google-analytics/astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  srcDir: './demo',
   server: {
     port: 2330,
   },
